eda687a137cdca32f74a11aff06ed36da3e7a1e3
Suraj R <surajrindia@gmail.com>
2018-09-23 22:58:39 +0530

    recomit

---
+++ Chapter1/ex3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 23 21:55:00 2018
+
+@author: Suraj-Desktop
+"""
+
+# ist way to make dictionaries
+bob = {'name': 'Bob Smith', 'age':42, 'pay':30000, 'job':'dev'}
+sue = {'name': 'Sue Jones', 'age':42, 'pay':40000, 'job':'hdw'}
+
+#print(bob['name'],sue['pay'])
+
+# second way to make dictionares
+
+bob = dict(name = 'Bob Smith', age=42, pay=30000, job='dev')
+sue = dict(name = 'Sue Jones', age=45, pay=40000, job='hwd')
+
+print (bob, sue)
