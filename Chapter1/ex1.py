33caa4ad26a0f77636fdfebbe870fa366a5d8bfb
Suraj R <surajrindia@gmail.com>
2018-09-23 21:34:38 +0530

    commit2 : database and field labels

--- Chapter1/ex1.py
+++ Chapter1/ex1.py
@@ -18,6 +18,33 @@ sue[2] *= 1.25
 # a database list
 
 people = [bob, sue]
-for person in people:
-    print(person)
+#for person in people:
+    #print(person)
 
+#print (people[1][0])
+    
+for person in people:
+    #print(person[0].split()[-1])
+    person[2] *= 1.2
+    
+#print (people)
+    
+# lists maps ans generators
+
+#pays = [person[2] for person in people] # example of list
+
+#pays = map((lambda x:x[2]),people)      # example of map
+#pays = list(pays) 
+    
+#pays = sum(person[2] for person in people) # example of generator expressions
+    
+#print(pays)
+    
+people.append(['Tom', 50, 0, None])
+#print(people[-1])
+
+# Field labels
+
+Name, age, pay = range(3)
+bob = ['Bob Smith', 42, 10000]
+print(bob[Name],bob[pay])
