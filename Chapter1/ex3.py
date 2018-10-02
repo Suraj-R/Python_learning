e6bf3f16a52a45b88dfe3cc31b2a90c86a8f6a82
Suraj R <surajrindia@gmail.com>
2018-10-02 13:27:00 +0530

    commit : dictionaries

--- Chapter1/ex3.py
+++ Chapter1/ex3.py
@@ -16,4 +16,37 @@ sue = {'name': 'Sue Jones', 'age':42, 'pay':40000, 'job':'hdw'}
 bob = dict(name = 'Bob Smith', age=42, pay=30000, job='dev')
 sue = dict(name = 'Sue Jones', age=45, pay=40000, job='hwd')
 
-print (bob, sue)
+#print (bob, sue)
+
+#print(bob['name'], sue['pay'])
+
+sue = {}
+sue['name'] = 'Sue Jones'
+sue['age'] = 45
+sue['pay'] = 40000
+sue['job'] = 'hdw'
+
+#print(sue)
+
+names = ['name', 'age', 'pay', 'job']
+values = ['Sue Jones', 45, 40000, 'hdw']
+sue = dict(zip(names,values))
+#print(sue)
+
+#empty dictionary initialization
+
+fields = ('name', 'age', 'pay', 'job')
+record = dict.fromkeys(fields, 0)
+#print(record)
+
+people = [bob, sue, record]
+#print (people)
+
+"""for person in people:
+    if person['name'] == 'Sue Jones':
+        print (person['pay'])"""
+        
+names = [person['name'] for person in people]
+
+names = list(map(lambda x:x['name'],people))
+print (names)
